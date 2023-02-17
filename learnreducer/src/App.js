b6dacef505635983a5507adba0749dff20edafb3
mahmoudbk <mahmoudbechki@gmail.com>
2023-02-17 11:21:41 +0100

    a very simple way of using useReducer

--- learnreducer/src/App.js
+++ learnreducer/src/App.js
@@ -1,19 +1,33 @@
 import { useReducer } from "react";
 
-
+const ACTIONS = {
+  INCREMENT:"increment",
+  DECREMENT:"decrement"
+}
 function reducer(state,action){
-  return {count:state.count +1}
+  switch (action.type){
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1}
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1}
+    default:
+      return state
+  }
+  
 }
 function App() {
 
 
   function incremenet(){
-    dispatch();
+    dispatch({type:ACTIONS.INCREMENT});
+  }
+  function decrement(){
+    dispatch({type:ACTIONS.DECREMENT})
   }
   const [state,dispatch] = useReducer(reducer,{count:0})
   return (
     <div>
-      <button>-</button>
+      <button onClick={decrement}>-</button>
       <h1>{state.count}</h1>
       <button onClick={incremenet}>+</button>
     </div>
