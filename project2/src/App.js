0f485afb7c597b6e2a146249d7a1044dd6a285c4
mahmoudbk <mahmoudbechki@gmail.com>
2023-02-19 18:12:58 +0100

    a working observer that fetches data at a certain intersection

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,10 +1,14 @@
 
-import { useState } from "react";
+import { useState,useRef, useCallback } from "react";
 import useBookSearch from "./useBookSearch";
 function App() {
   const [query, setQuery] = useState('');
   const [pageNumber, setPageNumber] = useState(1);
  
+
+
+
+
   function handleChange(e){
     setQuery(e.target.value);
     setPageNumber(1);
@@ -12,11 +16,34 @@ function App() {
     
   }
   const {books, hasMore,loading,error} = useBookSearch(query, pageNumber);
+  
+  
+  const observer =useRef();
+  const lastBookElementRef = useCallback((node)=> {
+    //if we are still loading don't do anything
+    if (loading) return 
+    //disconnecting from the previous observer in order to set a new one
+
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore){
+       setPageNumber(prv=> prv+1)
+      }
+    })
+
+    if(node) observer.current.observe(node);
+  }, [loading, hasMore]);
+  
   return (
     <>
    <input type="text" value={query} onChange={handleChange}></input>
-   {books.map(book=> {
-    return <div key={book}>{book}</div>
+   {books.map((book,index)=> {
+    if(books.length===index+1){
+      return <div ref={lastBookElementRef} key={book}>{book}</div>
+    }else {
+      return <div key={book}>{book}</div>
+    }
+    
    })}
    
    <div>{loading && 'Loadin...'}</div>
