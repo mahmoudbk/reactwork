4d8399d3dca98686360ae9be49ca7a37201ae384
mahmoudbk <mahmoudbechki@gmail.com>
2023-02-23 12:00:03 +0100

    mission canceled

--- currencyconvertor/src/App.js
+++ currencyconvertor/src/App.js
@@ -1,7 +1,38 @@
 
+import { useState,useEffect } from "react";
+
+import CurrencyRow from "./CurrencyRow";
+import axios from "axios";
+
+
+
 function App() {
+  const [currency,setCurrencyData] = useState(null);
+  const fromCurrency = "USD";
+  const toCurrency = "EUR";
+  useEffect(() => {
+   
+
+    axios.get(`https://openexchangerates.org/api/convert/${fromCurrency}/${toCurrency}`)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+
+  
   return (
-    <h1>Hello world</h1>
+    <>
+    <div className="container">
+    <h1>Convert currency</h1>
+    <CurrencyRow/>
+    <div>==</div>
+    <CurrencyRow/>
+    </div>
+    </>
   );
 }
 
---
+++ currencyconvertor/src/CurrencyRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CurrencyRow() {
+  return (
+    <div>
+        <input type="number" />
+        <select>
+            <option>hi</option>
+        </select>
+    </div>
+  )
+}
